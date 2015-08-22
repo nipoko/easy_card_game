3d721a2f529e85dbc511a8f22148f6aa6c564c6a
nipoko <hachi@8-degrees.info>
2015-08-22 17:14:21 +0900

    ゲームシステムとプレイヤーをクラス分け

--- cardgame.rb
+++ cardgame.rb
@@ -26,28 +26,64 @@ class CardGame
     @hand[:player1].sort!
     @hand[:player2].sort!
 
+    # プレイヤー生成
+    @player1 = Player.new(:player1, @hand[:player1])
+    @player2 = Player.new(:player2, @hand[:player2])
+    @turn_player = @player1
+
+    # 捨て札置き場とポイントの初期化
     @discard_pile = {player1: [], player2: []}
     @point = {player1: 0, player2: 0}
   end
 
-  attr_reader :card_pile, :hand, :discard_pile, :point
+  attr_reader :card_pile, :hand, :discard_pile, :point, :turn_player
+  attr_accessor :player1, :player2
 
   def main
-    HAND_NUM.times do
-      self.put_result
-      result = self.cpu_turn(:player1)
-      unless result.nil? then
-        return result
+    # ラウンド処理
+    begin
+      wins = self.turn(@turn_player)
+      if wins then
+        puts "#{turn_player.side} wins!"
+        return turn_player.side
       end
-      puts
       self.put_result
-      result = self.cpu_turn(:player2)
-      unless result.nil? then
-        return result
-      end
-      puts
-    end
+      self.switch_player(@turn_player)
+    end until @hand[:player1].empty? and @hand[:player2].empty?
 
+    # 終了処理
+    return self.game_end
+  end
+
+  def turn(player)
+    discard = player.turn(@discard_pile)
+    # 捨て札処理
+    self.discard(player, discard)
+    # ポイント獲得処理
+    self.get_point(player, discard)
+    # 勝利判定
+    return self.victory?(player)
+  end
+
+  def discard(player, card)
+    @discard_pile[player.side] << card
+  end
+
+  def get_point(player, card)
+    if @discard_pile[player.side].count(card) + @discard_pile[player.opponent].count(card) == 3 then
+      @point[player.side] += 1
+    end
+  end
+
+  def victory?(player)
+    if @point[player.side] >= WIN_POINT then
+      return true
+    else
+      return false
+    end
+  end
+
+  def game_end
     remain_card = @card_pile[0]
     puts "remain card bonus! => #{remain_card}"
     if @discard_pile[:player1].rindex(remain_card) == nil then
@@ -64,51 +100,9 @@ class CardGame
       winner = :player2
     end
 
-    puts "#{winner} wins!"
+    puts "#{winner} wins!!"
     self.put_result
     return winner
-  end
-
-  def player_turn(side)
-    # 捨て札選択
-    begin
-      hand = @hand[side]
-      puts "#{side}: your hand: #{hand}"
-      print "#{side}: which card do you discard?: "
-      discard = gets.to_i
-    end while @hand[side].find_index(discard).nil?
-    # 捨て札処理
-    @hand[side].delete_at(@hand[side].find_index(discard))
-    @discard_pile[side] << discard
-    # ポイント獲得処理
-    if @discard_pile[side].count(discard) + @discard_pile[opponent(side)].count(discard) == 3 then
-      @point[side] += 1
-    end
-    if @point[side] >= WIN_POINT then
-      puts "Player1 wins!"
-      self.put_result
-      return side
-    end
-
-    return nil
-  end
-
-  def cpu_turn(side, mode = "normal")
-    discard = (mode == "kuso") ? self.cpu_ai_kuso(side) : self.cpu_ai(side)
-    @hand[side].delete_at(@hand[side].find_index(discard))
-    @discard_pile[side] << discard
-    puts "#{side}: discard a card: #{discard}"
-    # ポイント獲得処理
-    if @discard_pile[side].count(discard) + @discard_pile[opponent(side)].count(discard) == 3 then
-      @point[side] += 1
-    end
-    if @point[side] >= WIN_POINT then
-      puts "#{side} wins!"
-      self.put_result
-      return side
-    end
-
-    return nil
   end
 
   def put_result
@@ -122,24 +116,64 @@ class CardGame
     puts
   end
 
-  def cpu_ai(side)
-    uniq_list = @hand[side].uniq
-    my_cards = @hand[side].dup.concat(@discard_pile[side])
-    all_cards = my_cards.dup.concat(@discard_pile[opponent(side)])
+  def switch_player(player)
+    @turn_player = case player.side
+    when :player1 then @player2
+    when :player2 then @player1
+    end
+  end
+end
+
+class Player
+  def initialize(side, hand)
+    @side = side
+    @hand = hand
+    @mode = :cpu
+  end
+
+  attr_reader :side, :hand
+  attr_accessor :mode
+
+  def turn(discard_pile)
+    discard = case @mode
+    when :play then self.play
+    when :kuso_cpu then self.cpu_ai_kuso
+    else self.cpu_ai(discard_pile)
+    end
+    @hand.delete_at(@hand.find_index(discard))
+    puts "#{@side}: discard a card: #{discard}"
+
+    return discard
+  end
+
+  def play
+    begin
+      puts "#{@side}: your hand: #{@hand}"
+      print "#{@side}: which card do you discard?: "
+      card = gets.to_i
+    end while @hand.find_index(card).nil?
+
+    return card
+  end
+
+  def cpu_ai(discard_pile)
+    uniq_list = @hand.uniq
+    my_cards = @hand.dup.concat(discard_pile[@side])
+    all_cards = my_cards.dup.concat(discard_pile[self.opponent])
     priority = Hash.new
 
     uniq_list.each do |n|
       priority[n] = case true
       when all_cards.count(n) == 3 then 1000
-      when my_cards.count(n) == 2 && @hand[side].count(n) == 2 then 500
+      when my_cards.count(n) == 2 && hand.count(n) == 2 then 500
       when my_cards.count(n) == 2 then 10
-      when @hand[side].count(n) == 1 && @discard_pile[opponent(side)].count(n) == 0 then 200
-      when @hand[side].count(n) == 1 then -10
+      when hand.count(n) == 1 && discard_pile[self.opponent].count(n) == 0 then 200
+      when hand.count(n) == 1 then -10
       else 0
       end
     end
 
-    puts "debug: #{side} cpu_priority: #{priority}"
+    puts "debug: #{@side} cpu_priority: #{priority}"
 
     return priority.sort_by{|k, v| v}.last[0].to_i
   end
@@ -148,9 +182,10 @@ class CardGame
     return @hand[side].sample
   end
 
-  def opponent(side)
-    if side == :player1 then return :player2
-    else return :player1
+  def opponent
+    return case @side
+    when :player1 then :player2
+    when :player2 then :player1
     end
   end
 end
